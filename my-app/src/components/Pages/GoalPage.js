5b65a0d9492c607d2584ec9ccccec0a58e6d99e8
gaganj007 <gaganj2002@gmail.com>
2021-12-31 23:27:38 -0500

    More changes

--- my-app/src/components/Pages/GoalPage.js
+++ my-app/src/components/Pages/GoalPage.js
@@ -1,8 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import Container from '@material-ui/core/Container'
+import { TextField } from '@material-ui/core';
+import {makeStyles} from '@material-ui/core';
+import {Button} from '@material-ui/core';
+import { IconButton } from "@material-ui/core";
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add'
+import Icon from '@material-ui/core/Icon'
+import { useNavigate } from "react-router-dom";
+
+
+const useStyles = makeStyles((theme) =>  ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+        },
+        button: {
+            margin: theme.spacing(1),
+        }
+    }
+    
+}))
 
 function Goal() {
+    let navigate = useNavigate();
+
+    const classes = useStyles()
+    const [inputFields, setInputField] = useState([
+        {goal: ''},
+    ]);
+
+    const handleChangeInput = (index,event) => {
+        const values = [...inputFields];
+        values[index][event.target.name] = event.target.value;
+        setInputField(values);
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("InputFields",inputFields);
+        navigate("/recommendation-page")
+
+    };
+
+    const handleAddFields = () => {
+        setInputField([...inputFields, { goal: ''}])
+    }
+
+    const handleRemoveFields = (index) => {
+        const values =[...inputFields]
+        values.splice(index, 1);
+        setInputField(values);
+    }
     return (
-        <div>This page is to goals</div>
+        <Container>
+            <h1>Add a Goal</h1>
+            <form className={classes.root} onSubmit={handleSubmit}>
+                { inputFields.map((inputField, index) => (
+                    <div key={index}>
+                        <TextField
+                            name="goal"
+                            label="GOAL"
+                            value={inputField.goal}
+                            onChange={event => handleChangeInput(index,event)}
+                        />
+
+                        <IconButton
+                            onClick={() => handleRemoveFields(index)}
+                        >
+                            <RemoveIcon />
+                        </IconButton>
+                        <IconButton
+                            onClick={() => handleAddFields()}
+                        >
+                            <AddIcon/>
+                        </IconButton>
+                    </div>
+
+                ))}
+                <Button className={classes.button} variant="contained" color ="primary" type="submit" onClick={handleSubmit}>Done!</Button>
+            </form>
+        </Container>
     )
 }
 
